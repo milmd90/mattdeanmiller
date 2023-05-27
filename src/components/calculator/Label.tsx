0d8f7004b647c273e93533f4bc64428a2f27b71c
Matthew Miller <milmd90@gmail.com>
2023-05-27 15:26:12 -0700

    add label

---
+++ src/components/calculator/Label.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function Label() {
+  return (
+    <div className='label'>
+      <div className='tab-column'>
+        <div>
+          e
+        </div>
+        <div>
+          B
+        </div>
+        <div>
+          G
+        </div>
+        <div>
+          D
+        </div>
+        <div>
+          A
+        </div>
+        <div>
+          E
+        </div>
+      </div>
+      <div className='input-column'>
+        <div>
+          root
+        </div>
+        <div>
+          type
+        </div>
+        <div>
+          shape
+        </div>
+        <div>
+          position
+        </div>
+        <div>
+          option
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Label;
