af7b20ca00b16da34d6f732ac0b8d2124375cd47
Matthew Miller <milmd90@gmail.com>
2023-07-26 17:34:57 -0700

    add Color Key

---
+++ src/components/calculator/ColorKey.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const definedTones = [{
+  degree: "root",
+  offset: 0,
+}, {
+  degree: "minor second",
+  offset: 1,
+}, {
+  degree: "major second",
+  offset: 2,
+}, {
+  degree: "minor third",
+  offset: 3,
+}, {
+  degree: "major third",
+  offset: 4,
+}, {
+  degree: "perfect forth",
+  offset: 5,
+}, {
+  degree: "tritone",
+  offset: 6,
+}, {
+  degree: "perfect fifth",
+  offset: 7,
+}, {
+  degree: "minor sixth",
+  offset: 8,
+}, {
+  degree: "major sixth",
+  offset: 9,
+}, {
+  degree: "minor seventh",
+  offset: 10,
+}, {
+  degree: "major seventh",
+  offset: 11,
+}];
+
+function ColorKey(props: {
+  showColors: boolean,
+}) {
+  if (!props.showColors) return null;
+
+  return (
+    <div id="color-key">
+      <h3>Color Key</h3>
+      {definedTones.map(notes =>
+        <div>
+          <span className={`color-key-sample tone-${notes.offset}`}>&nbsp;</span>
+          <span>&nbsp;-&nbsp;</span>
+          <span>{notes.degree}</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ColorKey;
