4350b56594726e71b42ec55802d8be9df29e469f
Matthew Miller <milmd90@gmail.com>
2023-08-01 15:51:20 -0700

    no match error

--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function NoMatch(props) {
   return (
-    <div id='no-match' className="container">
-      Woopsie
+    <div id='no-match' className="container page">
+      <h2>Error</h2>
+      <div className='section'>
+        <div className='item'>
+          <Link to='/'>
+            Click to return home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
