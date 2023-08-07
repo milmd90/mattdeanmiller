c1c23781f14dc2818c79c7ed48eb8bc49709728d
Matthew Miller <milmd90@gmail.com>
2023-08-07 12:44:46 -0700

    small

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,10 +13,10 @@ function Home(props) {
             I'm Matthew Miller, a music producer, audio engineer, and musician in Los Angeles.
           </p>
           <p>
-            I plan to use this site to share projects I work on as well as tools for musicians and engineers.
+            I plan to use this site to share projects I work on, as well as tools for musicians and engineers.
           </p>
           <p>
-            This site is currently under development, so please visit again to see what features and content has been added! 
+            This site is currently under development, so please visit again to see what features and content have been added! 
           </p>
           <p>
             All business inqueries can be sent to me at mattdeanmiller@gmail.com
