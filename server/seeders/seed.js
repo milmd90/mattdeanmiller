20d611ac7d32e51b2ea2a5e5051796314921c092
Matthew Miller <milmd90@gmail.com>
2023-08-11 16:13:21 -0700

    all permutations for all shapes

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -2,23 +2,87 @@ const db = require('../config/connection');
 const { Chord } = require('../models');
 const chords = require('./chordSeeds.json');
 
+
 let completeDictionary = [];
+
+const createPermutations = async (chord) => {
+  const { 
+    type,
+    shape,
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    sixth
+  } = chord;
+  
+  completeDictionary.push({
+    type,
+    shape,
+    frets: [
+      first,
+      second, 
+      third, 
+      fourth, 
+      fifth, 
+      null,
+    ]
+  });
+  completeDictionary.push({
+    type,
+    shape,
+    frets: [
+      first,
+      second, 
+      third, 
+      fourth, 
+      null, 
+      null,
+    ]
+  });
+  completeDictionary.push({
+    type,
+    shape,
+    frets: [
+      null,
+      second, 
+      third, 
+      fourth, 
+      fifth, 
+      null,
+    ]
+  });
+  completeDictionary.push({
+    type,
+    shape,
+    frets: [
+      first,
+      second, 
+      third, 
+      null, 
+      null, 
+      sixth,
+    ]
+  });
+  completeDictionary.push({
+    type,
+    shape,
+    frets: [
+      null,
+      second, 
+      third, 
+      fourth, 
+      null, 
+      sixth,
+    ]
+  });
+}
+
 const createDictionary = async (chords) => {
 
   try {
     chords.forEach((chord) => {
-      const { 
-        type,
-        shape,
-        first,
-        second,
-        third,
-        fourth,
-        fifth,
-        sixth
-      } = chord;
-
-
       completeDictionary.push({
         type: chord.type,
         shape: chord.shape,
@@ -33,241 +97,14 @@ const createDictionary = async (chords) => {
       });
 
       if (
-        first !== null &&
-        second !== null &&
-        third !== null &&
-        fourth !== null &&
-        fifth !== null &&
-        sixth !== null
+        chord.first !== null &&
+        chord.second !== null &&
+        chord.third !== null &&
+        chord.fourth !== null &&
+        chord.fifth !== null &&
+        chord.sixth !== null
       ) {
-        switch(shape) {
-          case 'C':
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                null, 
-                null, 
-                sixth,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                sixth,
-              ]
-            });
-            return;
-          case 'A':
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            return;
-          case 'G':
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                sixth,
-              ]
-            });
-            return;
-          case 'E':
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                sixth,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            return;
-          case 'D':
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                null,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                first,
-                second, 
-                third, 
-                fourth, 
-                null, 
-                sixth,
-              ]
-            });
-            completeDictionary.push({
-              type,
-              shape,
-              frets: [
-                null,
-                second, 
-                third, 
-                fourth, 
-                fifth, 
-                sixth,
-              ]
-            });
-            return;
-        }
+        createPermutations(chord);
       }
     })
 
