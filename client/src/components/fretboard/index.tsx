07d084a4b485ce8b80e098b1ab07f5d0fbf3dfdb
Matthew Miller <milmd90@gmail.com>
2023-09-03 17:34:38 -0700

    add frets to fretboard

--- client/src/components/fretboard/index.tsx
+++ client/src/components/fretboard/index.tsx
@@ -1,21 +1,23 @@
+function range(min: number, max: number) {
+  var len = max - min + 1;
+  var arr = [];
+  for (var i = 0; i < len; i++) {
+    arr[i] = min + i;
+  }
+  return arr;
+}
+
 function FretboardString(props: {
   start: number,
   end: number,
   string: string,
   frets: number[]
 }) {
-  function range(min: number, max: number) {
-    var len = max - min + 1;
-    var arr = [];
-    for (var i = 0; i < len; i++) {
-      arr[i] = min + i;
-    }
-    return arr;
-  }
-
   const frets = range(props.start, props.end).map((i) => {
     return <span className='fret-box'>
-      {props.frets.includes(i) ? i : ''}
+      <div className={`fret-value ${props.frets.includes(i) ? `tone-x` : ''}`}>
+        {props.frets.includes(i) ? i : ''}
+      </div>
     </span>
   })
 
@@ -25,6 +27,42 @@ function FretboardString(props: {
     </div>
   );
 }
+
+function FretboardMarkers(props: {
+  start: number,
+  end: number,
+  frets: number[]
+}) {
+  const frets = range(props.start, props.end).map((i) => {
+    return <span className='fret-box'>
+      <div className='fret-value'>
+        {props.frets.includes(i) ? i : ''}
+      </div>
+    </span>
+  })
+
+  return (
+    <div className="fret-markers">
+      {frets}
+    </div>
+  );
+}
+
+function Frets(props: {
+  start: number,
+  end: number,
+}) {
+  const frets = range(props.start, props.end).map((i) => {
+    return <span className='fret' />
+  })
+
+  return (
+    <div className="frets">
+      {frets}
+    </div>
+  );
+}
+
 
 export default function Fretboard(props: {
   start: number,
@@ -40,41 +78,54 @@ export default function Fretboard(props: {
 }) {
   return (
     <div className="fretboard">
-      <FretboardString
+      <div className="fretboard-content">
+        <Frets
+          start={props.start}
+          end={props.end}
+        />
+        <div className="fretboard-strings">
+          <FretboardString
+            start={props.start}
+            end={props.end}
+            string={'e'}
+            frets={props.frets['e']}
+          />
+          <FretboardString
+            start={props.start}
+            end={props.end}
+            string={'B'}
+            frets={props.frets['B']}
+          />
+          <FretboardString
+            start={props.start}
+            end={props.end}
+            string={'G'}
+            frets={props.frets['G']}
+          />
+          <FretboardString
+            start={props.start}
+            end={props.end}
+            string={'D'}
+            frets={props.frets['D']}
+          />
+          <FretboardString
+            start={props.start}
+            end={props.end}
+            string={'A'}
+            frets={props.frets['A']}
+          />
+          <FretboardString
+            start={props.start}
+            end={props.end}
+            string={'E'}
+            frets={props.frets['E']}
+          />
+        </div>
+      </div>
+      <FretboardMarkers
         start={props.start}
         end={props.end}
-        string={'e'}
-        frets={props.frets['e']}
-      />
-      <FretboardString
-        start={props.start}
-        end={props.end}
-        string={'B'}
-        frets={props.frets['B']}
-      />
-      <FretboardString
-        start={props.start}
-        end={props.end}
-        string={'G'}
-        frets={props.frets['G']}
-      />
-      <FretboardString
-        start={props.start}
-        end={props.end}
-        string={'D'}
-        frets={props.frets['D']}
-      />
-      <FretboardString
-        start={props.start}
-        end={props.end}
-        string={'A'}
-        frets={props.frets['A']}
-      />
-      <FretboardString
-        start={props.start}
-        end={props.end}
-        string={'E'}
-        frets={props.frets['E']}
+        frets={[3, 5, 7, 9, 12]}
       />
     </div>
   );
---
+++ client/src/helpers/fretboard.ts
@@ -0,0 +1,3 @@
+export function createFretboardData(): void  {
+
+}
